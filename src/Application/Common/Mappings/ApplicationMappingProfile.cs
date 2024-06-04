9ccf94b20846f5e2acec4f05bd3662cc7814341a
Blerim Sherifi <blerim_sherifi@live.com>
2024-06-04 14:57:36 +0200

    Refactor entity mapping and update namespaces
    In `ApplicationMappingProfile.cs`, added namespaces `Assignment.Application.TodoLists.Queries.GetTodos` and `Assignment.Domain.Entities`. Created a new class `ApplicationMappingProfile` inheriting from `Profile` and defined mappings for `TodoList` to `TodoListDto` and `TodoItem` to `TodoItemDto`. In `LookupDto.cs`, removed the namespace `Assignment.Domain.Entities` and the private class `Mapping` that was previously responsible for mapping `TodoList` and `TodoItem` to `LookupDto`. This indicates a shift in responsibility for these mappings to the `ApplicationMappingProfile` class.

---
+++ src/Application/Common/Mappings/ApplicationMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using Assignment.Application.TodoLists.Queries.GetTodos;
+using Assignment.Domain.Entities;
+
+namespace Assignment.Application.Common.Mappings;
+internal class ApplicationMappingProfile : Profile
+{
+    public ApplicationMappingProfile()
+    {
+        CreateMap<TodoList, TodoListDto>();
+        CreateMap<TodoItem, TodoItemDto>();
+    }
+}
